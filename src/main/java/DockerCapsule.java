15bb0e5a90b9bf6e21b5ea82ba5b6aef8c11cba2
pron <ron.pressler@gmail.com>
2014-10-23 17:54:03 +0300

    initial commit

---
+++ src/main/java/DockerCapsule.java
@@ -0,0 +1,19 @@
+import java.nio.file.Path;
+
+
+/*
+ * Copyright (c) 2014, Parallel Universe Software Co. and Contributors. All rights reserved.
+ * 
+ * This program and the accompanying materials are licensed under the terms 
+ * of the Eclipse Public License v1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ *
+ * @author pron
+ */
+public class DockerCapsule extends Capsule {
+    public DockerCapsule(Path jarFile, Path cacheDir) {
+        super(jarFile, cacheDir);
+    }
+}
